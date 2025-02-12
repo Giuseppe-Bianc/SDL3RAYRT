3355b530548a0e10510a5d73da0ae526b02f8e55
GIUSEPPE BIANCONI <gbianconi3@gmail.com>
2025-02-12 21:27:56 +0100

    Update CMake configuration and improve project settings

--- cmake/StandardProjectSettings.cmake
+++ cmake/StandardProjectSettings.cmake
@@ -1,45 +1,58 @@
 # Set a default build type if none was specified
-if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
+if (NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
   message(STATUS "Setting build type to 'RelWithDebInfo' as none was specified.")
   set(CMAKE_BUILD_TYPE
-      RelWithDebInfo
-      CACHE STRING "Choose the type of build." FORCE)
+          RelWithDebInfo
+          CACHE STRING "Choose the type of build." FORCE)
   # Set the possible values of build type for cmake-gui, ccmake
   set_property(
-    CACHE CMAKE_BUILD_TYPE
-    PROPERTY STRINGS
-             "Debug"
-             "Release"
-             "MinSizeRel"
-             "RelWithDebInfo")
-endif()
+          CACHE CMAKE_BUILD_TYPE
+          PROPERTY STRINGS
+          "Debug"
+          "Release"
+          "MinSizeRel"
+          "RelWithDebInfo")
+endif ()
 
 # Generate compile_commands.json to make it easier to work with clang based tools
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
 # Enhance error reporting and compiler messages
-if(CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
-  if(WIN32)
-    # On Windows cuda nvcc uses cl and not clang
+if (CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
+  if (WIN32)
+    # On Windows, CUDA nvcc uses cl and not clang
     add_compile_options($<$<COMPILE_LANGUAGE:C>:-fcolor-diagnostics> $<$<COMPILE_LANGUAGE:CXX>:-fcolor-diagnostics>)
-  else()
+  else ()
     add_compile_options(-fcolor-diagnostics)
+  endif ()
+  message(STATUS "Using Clang compiler.")
+elseif (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+  if(CMAKE_VERSION VERSION_LESS 3.24)
+    check_cxx_compiler_flag(-fdiagnostics-color flag_color_diag)
+    if(flag_color_diag)
+      if (WIN32)
+        # On Windows, CUDA nvcc uses cl and not GCC
+        add_compile_options($<$<COMPILE_LANGUAGE:C>:-fdiagnostics-color=always>
+                $<$<COMPILE_LANGUAGE:CXX>:-fdiagnostics-color=always>)
+      else ()
+        add_compile_options(-fdiagnostics-color=always)
+      endif ()
+    endif()
+  elseif(NOT DEFINED ENV{CMAKE_COLOR_DIAGNOSTICS})
+    set(CMAKE_COLOR_DIAGNOSTICS ON)
   endif()
-elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-  if(WIN32)
-    # On Windows cuda nvcc uses cl and not gcc
-    add_compile_options($<$<COMPILE_LANGUAGE:C>:-fdiagnostics-color=always>
-                        $<$<COMPILE_LANGUAGE:CXX>:-fdiagnostics-color=always>)
-  else()
-    add_compile_options(-fdiagnostics-color=always)
-  endif()
-elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC" AND MSVC_VERSION GREATER 1900)
+  message(STATUS "Using GCC compiler.")
+elseif (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC" AND MSVC_VERSION GREATER 1900)
   add_compile_options(/diagnostics:column)
-else()
+  message(STATUS "Using MSVC compiler with column diagnostics.")
+else ()
   message(STATUS "No colored compiler diagnostic set for '${CMAKE_CXX_COMPILER_ID}' compiler.")
-endif()
+endif ()
 
 
-# run vcvarsall when msvc is used
+# Run vcvarsall when MSVC is used
 include("${CMAKE_CURRENT_LIST_DIR}/VCEnvironment.cmake")
 run_vcvarsall()
+
+# Additional informative messages
+message(STATUS "Compile commands exported to: ${CMAKE_BINARY_DIR}/compile_commands.json")
